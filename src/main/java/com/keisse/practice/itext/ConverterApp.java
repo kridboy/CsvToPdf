84992273f9204e33565cbab13f2bd19b3e5422d6
kridboy <keisse.m@hotmail.com>
2019-09-28 17:27:10 -0700

    final large refactor

---
+++ src/main/java/com/keisse/practice/itext/ConverterApp.java
@@ -0,0 +1,11 @@
+package com.keisse.practice.itext;
+
+import java.io.IOException;
+
+public class ConverterApp {
+    public static void main(String[] args) throws IOException {
+        CsvToPdfConverter converter = new CsvToPdfConverter("input");
+        converter.convertFiles();
+        long endTime = System.nanoTime();
+    }
+}
--- src/main/java/com/keisse/practice/itext/CsvToPdfConverter.java
+++ src/main/java/com/keisse/practice/itext/CsvToPdfConverter.java
@@ -3,44 +3,84 @@ package com.keisse.practice.itext;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Cell;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.element.Text;
+import com.itextpdf.layout.property.HorizontalAlignment;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class CsvToPdfConverter {
-    private static final File INPUT_FOLDER = new File("input");
+    private static final String REGEX = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)|(\r\n|\n)";
     private static final String CSV_EXTENSION = ".csv";
+    private static final float FONT_SIZE = 5f;
+    private final File inputFolder;
 
-    public static void main(String[] args) throws IOException{
-        CsvToPdfConverter csvToPdfConverter = new CsvToPdfConverter();
-        csvToPdfConverter.convertFiles();
+    public CsvToPdfConverter(String folderName) {
+        inputFolder = new File(folderName);
+        new File("pdf").mkdir();
     }
 
-    public void convertFiles() throws IOException{
-        for(ParsedCsvTable parsedCsvTable : createParsedTablesFromFiles()){
-            PdfWriter writer = new PdfWriter("pdf/"+ parsedCsvTable.getTitle() + ".pdf");
-            PdfDocument pdfDocument = new PdfDocument(writer);
-            Document document = new Document(pdfDocument);
-            document.add(parsedCsvTable.getPdfTable());
-            document.close();
-            pdfDocument.close();
-            writer.close();
+    public void convertFiles() throws IOException {
+        for (Map.Entry<String, Table> entrySet : createCsvTableMap().entrySet()) {
+            try (PdfWriter writer = new PdfWriter("pdf/" + entrySet.getKey() + ".pdf");
+                 PdfDocument pdfDocument = new PdfDocument(writer);
+                 Document document = new Document(pdfDocument)) {
+                document.add(entrySet.getValue());
+            }
         }
     }
 
-    private List<ParsedCsvTable> createParsedTablesFromFiles() {
-        List<ParsedCsvTable> output = new ArrayList<>();
-        if (INPUT_FOLDER.isDirectory() && INPUT_FOLDER.listFiles() != null) {
-            for(File inputFile:INPUT_FOLDER.listFiles()){
+    private Map<String, Table> createCsvTableMap() throws IOException {
+        Map<String, Table> csvMap = new HashMap<>();
+        if (inputFolder.isDirectory() && inputFolder.listFiles() != null) {
+            for (File inputFile : inputFolder.listFiles()) {
                 String extension = inputFile.getName();
                 extension = extension.substring(extension.lastIndexOf('.'));
 
                 if (extension.equals(CSV_EXTENSION)) {
-                    output.add(new ParsedCsvTable(inputFile));
+                    String fileName = inputFile.getName().substring(0, inputFile.getName().lastIndexOf('.'));
+                    csvMap.put(fileName, processDataToTable(inputFile));
                 }
             }
         }
-        return output;
+        return csvMap;
     }
+
+    private Table processDataToTable(File inputFile) throws IOException {
+        Queue<String> dataQueue = new LinkedList<>();
+        try (Scanner scanner = new Scanner(new FileReader(inputFile))) {
+            while (scanner.hasNext()) {
+                dataQueue.offer(scanner.nextLine());
+            }
+        }
+        String[] headerNames = dataQueue.poll().split(",");
+        Table pdfTable = new Table(headerNames.length).setHorizontalAlignment(HorizontalAlignment.CENTER);
+
+        for (String str : headerNames)
+            pdfTable.addCell(new Cell().add(new Paragraph(new Text(trimCellInput(str)).setFontSize(FONT_SIZE))));
+
+        Queue<String> tableData = new LinkedList<>();
+        while (!dataQueue.isEmpty()) {
+            String row = dataQueue.poll();
+            String[] tableRow = row.split(REGEX);
+
+            for (String str : tableRow)
+                tableData.offer(str);
+
+            while (!tableData.isEmpty())
+                pdfTable.addCell(new Cell().add(new Paragraph(new Text(trimCellInput(tableData.poll())).setFontSize(FONT_SIZE))));
+        }
+        return pdfTable;
+    }
+
+    private String trimCellInput(String str) {
+        str = str.trim();
+        return str.startsWith("\"") ? str.substring(1, str.length() - 1) : str;
+    }
+
 }
